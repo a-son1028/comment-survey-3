5af11a82eefd24aa7589a9521bbabd8edf7b128a
tuan <lethanhtuan1028@gmail.com>
2022-03-16 22:23:23 +0700

    update ui question

--- src/views/question/Question.vue
+++ src/views/question/Question.vue
@@ -9,13 +9,16 @@
       style="text-transform: capitalize;"
     >Security And Privacy Comments Survey</h3>
 
-    <div class="mt-3"><b>Comment:</b> I don't understand why this app requires the all permission to add or modify calendar events ...</div>
+    <div
+      class="mt-3"
+      style="font-size: 18px"
+    ><b>Comment:</b> I don't understand why this app requires the all permission to add or modify calendar events ...</div>
 
     <div id="questions">
 
       <!-- question 1 -->
       <div class="mt-2">
-        <div class="ml-3">1. Does this comment include the information related to security and privacy issues?</div>
+        <div class="ml-3">1. Is this comment about the security and privacy issues of the app?</div>
         <UIRadioGroup
           v-model="question1"
           name="question1"
@@ -64,17 +67,34 @@
         <div
           class="mt-2"
         >
-          <div class="ml-3">2. What is the sentiment of the security and privacy-related comments?</div>
+          <div class="ml-3">2. What is the sentiment of the comment on security?</div>
           <UIRadioGroup
             v-model="question2"
             name="question2"
             :options="question2Options"
           />
+
+          <!-- if 2 yes -->
+          <div 
+            v-if="question2 === 0 || question2 === 1 || question2 === 2"
+            class="if-yes"
+          >
+            Please copy the related content to the text:
+            <div class="anwsers">
+              <textarea
+                id=""
+                name="reason"
+                cols="30"
+                rows="5"
+                style="width: 100%"
+              />
+            </div>
+          </div>
         </div>
 
         <!-- question 3 -->
         <div class="mt-2">
-          <div class="ml-3">3. Does this comment mention the required permission?</div>
+          <div class="ml-3">3. What is the sentiment of the comment on privacy?</div>
           <UIRadioGroup
             v-model="question3"
             name="question3"
@@ -82,72 +102,27 @@
           />
 
           <!-- if 3 yes -->
-          <div v-if="question3 === 1">
-            <div class="ml-3">3.1. Does this comment mention the specific permission name?</div>
-            <UIRadioGroup
-              v-model="question31"
-              name="question31"
-              :options="question3Options"
-            />
-
-            <!-- if 3.1 yes -->
-            <div
-              v-if="question31 === 1"
-              class="if-yes"
-            >
-              Please select the related content to the text:
-              <div class="anwsers">
-                <label class="container-checkbox">camera
-                  <input
-                    class="type-question"
-                    type="checkbox"
-                  >
-                  <span class="checkmark" />
-                </label>
-
-                <label class="container-checkbox">notification
-                  <input
-                    class="type-question"
-                    type="checkbox"
-                  >
-                  <span class="checkmark" />
-                </label>
-
-                <label class="container-checkbox">calendar
-                  <input
-                    class="type-question"
-                    type="checkbox"
-                  >
-                  <span class="checkmark" />
-                </label>
-              </div>
-          
+          <div 
+            v-if="question3 === 0 || question3 === 1 || question3 === 2"
+            class="if-yes"
+          >
+            Please copy the related content to the text:
+            <div class="anwsers">
+              <textarea
+                id=""
+                name="reason"
+                cols="30"
+                rows="5"
+                style="width: 100%"
+              />
             </div>
-
-            <!-- if 3.1 no -->
-            <div 
-              v-if="question31 === 0"
-              class="if-no "
-            >
-              Please enter the reason:
-              <div class="anwsers">
-                <textarea
-                  id=""
-                  name="reason"
-                  cols="30"
-                  rows="5"
-                  style="width: 100%"
-                />
-              </div>
-            </div> 
-          
           </div>
         </div>
 
 
         <!-- question 4 -->
         <div class="mt-2">
-          <div class="ml-3">4. Does this comment mention the collected data item?</div>
+          <div class="ml-3">4. Does this comment mention permissions required by the app?</div>
           <UIRadioGroup
             v-model="question4"
             name="question4"
@@ -156,7 +131,7 @@
 
           <!-- if 4 yes -->
           <div v-if="question4 === 1">
-            <div class="ml-3">4.1. Does this comment mention the purpose of the data collection?</div>
+            <div class="ml-3">4.1. Does the comment refer to any the specific permissions?</div>
             <UIRadioGroup
               v-model="question41"
               name="question41"
@@ -211,7 +186,7 @@
 
         <!-- question 5 -->
         <div class="mt-2">
-          <div class="ml-3">5. Does this comment mention the shared data item?</div>
+          <div class="ml-3">5. Does this comment mention the collected data items?</div>
           <UIRadioGroup
             v-model="question5"
             name="question5"
@@ -220,7 +195,7 @@
 
           <!-- if 5 yes -->
           <div v-if="question5 === 1">
-            <div class="ml-3">5.1. Does this comment mention the purpose of data sharing?</div>
+            <div class="ml-3">5.1. Does it mention the purpose of data collection?</div>
             <UIRadioGroup
               v-model="question51"
               name="question51"
@@ -285,57 +260,95 @@
 
         <!-- question 6 -->
         <div class="mt-2">
-          <div class="ml-3">6. Does this comment mention the third party name?</div>
+          <div class="ml-3">6. Does this comment mention any sharing of data with third party?</div>
           <UIRadioGroup
             v-model="question6"
             name="question6"
             :options="question6Options"
           />
 
-          <!-- if yes -->
+          <!-- if 6 yes -->
           <div
             v-if="question6 === 1"
             class="if-yes"
           >
-            Please copy the related content to the text:
-            <div class="anwsers">
-              <textarea
-                id=""
-                name="reason"
-                cols="30"
-                rows="5"
-                style="width: 100%"
+            <div class="ml-3">6.1. Does it mention the purpose of data sharing?</div>
+            <UIRadioGroup
+              v-model="question61"
+              name="question61"
+              :options="question6Options"
+            />
+
+            <!-- if 6.1 yes -->
+            <div
+              v-if="question61 === 1"
+              class="if-yes"
+            >
+              <div class="ml-3">6.2. Does the comment refer to any the specific third parties?</div>
+              <UIRadioGroup
+                v-model="question62"
+                name="question62"
+                :options="question6Options"
               />
+
+              <!-- if yes -->
+              <div
+                v-if="question62 === 1"
+                class="if-yes"
+              >
+                Please copy the related content to the text:
+                <div class="anwsers">
+                  <textarea
+                    id=""
+                    name="reason"
+                    cols="30"
+                    rows="5"
+                    style="width: 100%"
+                  />
+                </div>
+          
+              </div>
+        
+              <!-- if 6.2 no -->
+              <div 
+                v-if="question62 === 0"
+                class="if-no "
+              >
+                Please enter the reason:
+                <div class="anwsers">
+                  <textarea
+                    id=""
+                    name="reason"
+                    cols="30"
+                    rows="5"
+                    style="width: 100%"
+                  />
+                </div>
+              </div> 
             </div>
+
+
+            <!-- if 6.1 no -->
+            <div 
+              v-if="question61 === 0"
+              class="if-no "
+            >
+              Please enter the reason:
+              <div class="anwsers">
+                <textarea
+                  id=""
+                  name="reason"
+                  cols="30"
+                  rows="5"
+                  style="width: 100%"
+                />
+              </div>
+            </div> 
           </div>
+          
         </div>
 
-        <!-- question 7 -->
-        <div class="mt-2">
-          <div class="ml-3">7. Does this comment mention the privacy assessment (e.g., the privacy is good)?</div>
-          <UIRadioGroup
-            v-model="question7"
-            name="question7"
-            :options="question7Options"
-          />
-
-          <!-- if yes -->
-          <div
-            v-if="question7 === 1"
-            class="if-yes"
-          >
-            Please copy the related content to the text:
-            <div class="anwsers">
-              <textarea
-                id=""
-                name="reason"
-                cols="30"
-                rows="5"
-                style="width: 100%"
-              />
-            </div>
-          </div>
-        </div>
+    
 
         
       </div>
@@ -354,12 +367,11 @@ import UINextButton from '@/components/UINextButton.vue'
 import UIRadioGroup from '@/components/UIRadioGroup.vue'
 
 const question1Options = [{label: 'Yes', value: 1}, {label: 'No', value: 0}, {label: 'Maybe', value: 2}]
-const question2Options = [{label: 'Positive', value: 1}, {label: 'Negative', value: 0}, {label: 'Neutral', value: 2}]
-const question3Options = [{label: 'Yes', value: 1}, {label: 'No', value: 0}]
-const question4Options = question3Options
-const question5Options = question3Options
-const question6Options = question1Options
-const question7Options = question1Options
+const question2Options = [{label: 'Positive', value: 1}, {label: 'Negative', value: 0}, {label: 'Neutral', value: 2}, {label: 'None', value: 3}]
+const question3Options = question2Options
+const question4Options = [{label: 'Yes', value: 1}, {label: 'No', value: 0}]
+const question5Options = question4Options
+const question6Options = question4Options
 
 export default {
   components: {
@@ -376,14 +388,14 @@ export default {
    question5: null,
    question51: null,
    question6: null,
-   question7: null,
+   question61: null,
+   question62: null,
    question1Options,
    question2Options,
    question3Options,
    question4Options,
    question5Options,
    question6Options,
-   question7Options
   }),
   watch: {
     question1(data) {
