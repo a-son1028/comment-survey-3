deb4995676a1c66f3804c426cc613fafe9126e8b
nb91265 <nb91265@navercorp.com>
2022-04-01 13:20:45 +0700

    fix

--- fe/src/services/api.js
+++ fe/src/services/api.js
@@ -1,13 +1,12 @@
 import axios from 'axios';
 
 const {
-  API_URL
+  VUE_APP_API_URL
 } = process.env
 
-console.log("API_URL", API_URL)
 export default function api({ headers }) {
   const instance = axios.create({
-    baseURL: API_URL,
+    baseURL: VUE_APP_API_URL,
     timeout: 60000,
     headers
   });
