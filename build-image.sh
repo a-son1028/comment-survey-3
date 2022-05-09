549d8f4192482cf8191b7ebc538e7348fe1a774f
nb91265 <nb91265@navercorp.com>
2022-05-09 21:09:26 +0700

    init source code

--- build-image.sh
+++ build-image.sh
@@ -1,6 +1,6 @@
-docker build --no-cache -t untadee/comment-survey-fe-2 ./fe
-docker push untadee/comment-survey-fe-2:latest
+docker build --no-cache -t untadee/comment-survey-fe-3 ./fe
+docker push untadee/comment-survey-fe-3:latest
 
 
-docker build --no-cache -t untadee/comment-survey-be-2 ./be
-docker push untadee/comment-survey-be-2:latest
+docker build --no-cache -t untadee/comment-survey-be-3 ./be
+docker push untadee/comment-survey-be-3:latest
