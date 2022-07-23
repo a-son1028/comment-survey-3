e00acaa500890eecb60d13840fe4d2f7ddfa259a
Tuan Le <lethanhtuan1028@gmail.com>
2022-07-23 10:41:19 +0700

    update

--- be/src/configs/mongoose.config.js
+++ be/src/configs/mongoose.config.js
@@ -2,20 +2,23 @@ const path = require("path");
 require("dotenv").config({ path: path.join(__dirname, "../../.env") });
 import mongoose from "mongoose";
 import Utils from "../utils";
+// import MongooseCache from "mongoose-redis";
 
-console.log("process.env.MONGODB_URL", process.env.MONGODB_URL)
+console.log("process.env.MONGODB_URL", process.env.MONGODB_URL);
 mongoose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+
+  poolSize: 100
 });
 
 mongoose.connection.on("error", err => {
-  console.log("Connect MONDGODB ERROR")
+  console.log("Connect MONDGODB ERROR");
   Utils.Logger.error(err);
 });
 
 mongoose.set("debug", true);
 
-
+// MongooseCache(mongoose, `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`);
